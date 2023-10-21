3e215982cb69df8b0f9c985ec84fe8be70124841
vikrantaroraa <arora.veshu@gmail.com>
2023-10-21 23:43:16 +0530

    feat: first commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="helloooooo"
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    // shadow for android
+    elevation: 6,
+    // shadow for ios
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    backgroundColor: "#c6e7d4",
+  },
+  numberInput: {
+    height: 50,
+    width: 60,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+});
+
+export default StartGameScreen;
